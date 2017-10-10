c99f26072f57aa4d268ecc2cce68a9e66a903851
DESKTOP-JB7VSRM\Jakub <bandaskorego@gmail.com>
2017-10-10 18:46:00 +0200

    Reausable drawer ready

--- app/src/main/java/com/school/jakub/trainingplanmaker/controller/MainActivity.java
+++ app/src/main/java/com/school/jakub/trainingplanmaker/controller/MainActivity.java
@@ -1,45 +1,43 @@
 package com.school.jakub.trainingplanmaker.controller;
 
+import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.school.jakub.trainingplanmaker.R;
 
 import io.realm.Realm;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+public class MainActivity extends NavDrawer {
 
     public static final String TAG = MainActivity.class.getName();
     private Realm myRealm;
-
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("");
-        setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open ,R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_main, null, false);
+        mDrawerLayout.addView(contentView, 0);
+
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Menu");
+
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
         myRealm = Realm.getDefaultInstance();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/school/jakub/trainingplanmaker/controller/NavDrawer.java
@@ -0,0 +1,83 @@
+package com.school.jakub.trainingplanmaker.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.school.jakub.trainingplanmaker.R;
+
+/**
+ * Created by Jakub on 09-Oct-17.
+ */
+
+public class NavDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    protected DrawerLayout mDrawerLayout;
+    protected NavigationView navigationView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        navigationView = (NavigationView) findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View hView =  navigationView.getHeaderView(0);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+       // Intent intent;
+
+        switch (item.getItemId()) {
+
+            case R.id.drawer_menu_account: {
+                System.out.println("drawer_menu_account");
+                break;
+            }
+            case R.id.drawer_menu_diary: {
+                System.out.println("drawer_menu_diary");
+                break;
+            }
+            case R.id.drawer_menu_training_plans: {
+                System.out.println("drawer_menu_training_plans");
+                break;
+            }
+            case R.id.drawer_menu_atlas: {
+                System.out.println("drawer_menu_atlas");
+                break;
+            }
+            case R.id.drawer_menu_calculators: {
+                System.out.println("drawer_menu_calculators");
+                break;
+            }
+            case R.id.drawer_menu_monitor: {
+                System.out.println("drawer_menu_monitor");
+                break;
+            }
+            case R.id.drawer_menu_weight: {
+                System.out.println("drawer_menu_weight");
+                break;
+            }
+            case R.id.drawer_menu_backpack: {
+                System.out.println("drawer_menu_backpack");
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
+        return true;
+    }
+}
